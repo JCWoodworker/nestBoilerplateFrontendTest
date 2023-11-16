1bb2b3df39bfe08522c1597770264d85966457b8
JCWoodworker <jc@rilocalwoodworks.com>
2023-11-15 21:59:05 -0500

    hardcoded prod domain to test

--- src/App.tsx
+++ src/App.tsx
@@ -5,14 +5,14 @@ import Login from "./pages/Login"
 
 function App() {
 	const [data, setData] = useState("")
-	const apiDomain =
-		import.meta.env.VITE_ENVIRONMENT === "development"
-			? import.meta.env.VITE_DEVELOPMENT_API_DOMAIN
-			: import.meta.env.VITE_ENVIRONMENT === "preprod"
-			? import.meta.env.VITE_PREPROD_API_DOMAIN
-			: import.meta.env.VITE_ENVIRONMENT === "production"
-			? import.meta.env.VITE_PRODUCTION_API_DOMAIN 
-			: import.meta.env.VITE_DEVELOPMENT_API_DOMAIN
+	const apiDomain = "https://nest-boilerplate-prod-2e4d0c59f648.herokuapp.com"
+		// import.meta.env.VITE_ENVIRONMENT === "development"
+		// 	? import.meta.env.VITE_DEVELOPMENT_API_DOMAIN
+		// 	: import.meta.env.VITE_ENVIRONMENT === "preprod"
+		// 	? import.meta.env.VITE_PREPROD_API_DOMAIN
+		// 	: import.meta.env.VITE_ENVIRONMENT === "production"
+		// 	? import.meta.env.VITE_PRODUCTION_API_DOMAIN 
+		// 	: import.meta.env.VITE_DEVELOPMENT_API_DOMAIN
 
 	const fetchMainPage = async () => {
 		const response = await axios.get(apiDomain)
