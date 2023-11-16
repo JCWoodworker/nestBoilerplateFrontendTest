e2d6c219c8ab07247d8d496a779af9c6ef87ff85
JCWoodworker <jc@rilocalwoodworks.com>
2023-11-15 22:09:25 -0500

    updated app

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,13 @@ import Login from "./pages/Login"
 
 function App() {
 	const [data, setData] = useState("")
-	const apiDomain = 
+	const apiDomain =
 		import.meta.env.VITE_ENVIRONMENT === "development"
 			? import.meta.env.VITE_DEVELOPMENT_API_DOMAIN
 			: import.meta.env.VITE_ENVIRONMENT === "preprod"
 			? import.meta.env.VITE_PREPROD_API_DOMAIN
 			: import.meta.env.VITE_ENVIRONMENT === "production"
-			? import.meta.env.VITE_PRODUCTION_API_DOMAIN 
+			? import.meta.env.VITE_PRODUCTION_API_DOMAIN
 			: undefined
 
 	const fetchMainPage = async () => {
