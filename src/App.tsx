ce567f9551fc7d832f6bfd07e4b4f007d5c39cfe
JCWoodworker <jc@rilocalwoodworks.com>
2023-11-15 22:04:11 -0500

    added console log for api domain as test

--- src/App.tsx
+++ src/App.tsx
@@ -5,19 +5,20 @@ import Login from "./pages/Login"
 
 function App() {
 	const [data, setData] = useState("")
-	const apiDomain = "https://nest-boilerplate-prod-2e4d0c59f648.herokuapp.com"
-		// import.meta.env.VITE_ENVIRONMENT === "development"
-		// 	? import.meta.env.VITE_DEVELOPMENT_API_DOMAIN
-		// 	: import.meta.env.VITE_ENVIRONMENT === "preprod"
-		// 	? import.meta.env.VITE_PREPROD_API_DOMAIN
-		// 	: import.meta.env.VITE_ENVIRONMENT === "production"
-		// 	? import.meta.env.VITE_PRODUCTION_API_DOMAIN 
-		// 	: import.meta.env.VITE_DEVELOPMENT_API_DOMAIN
+	const apiDomain = 
+		import.meta.env.VITE_ENVIRONMENT === "development"
+			? import.meta.env.VITE_DEVELOPMENT_API_DOMAIN
+			: import.meta.env.VITE_ENVIRONMENT === "preprod"
+			? import.meta.env.VITE_PREPROD_API_DOMAIN
+			: import.meta.env.VITE_ENVIRONMENT === "production"
+			? import.meta.env.VITE_PRODUCTION_API_DOMAIN 
+			: undefined
 
 	const fetchMainPage = async () => {
 		const response = await axios.get(apiDomain)
 		setData(response.data)
 	}
+	console.log(apiDomain)
 
 	useEffect(() => {
 		fetchMainPage()
